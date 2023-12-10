9fe7a13e46d374cb3f4fc475ff88167a742d41d5
Jacob Neil Taylor <me@jacobtaylor.id.au>
2023-12-10 13:07:04 +1100

    Update install_depot.sh

--- scripts/install_depot.sh
+++ scripts/install_depot.sh
@@ -2,6 +2,12 @@
 
 export DEPOT_ID=$1
 export DEPOT_DIR=/opt/steam/apps/$DEPOT_ID
+export STEAMCMD_USERNAME=${2:-anonymous}
+export STEAMCMD_PASSWORD=${3:-anonymous}
 
 mkdir -p $DEPOT_DIR
-steamcmd +force_install_dir $DEPOT_DIR +login anonymous +app_update $DEPOT_ID +quit
+if [ "$STEAMCMD_PASSWORD" = "anonymous" ]; then
+  steamcmd +force_install_dir $DEPOT_DIR +login $STEAMCMD_USERNAME +app_update $DEPOT_ID +quit
+else
+  steamcmd +force_install_dir $DEPOT_DIR +login $STEAMCMD_USERNAME $STEAMCMD_PASSWORD +app_update $DEPOT_ID +quit
+fi
